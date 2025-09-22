88c603e65afe9af8f0c7720fe9e26c530756cc0e
허윤 <huryoun5022@naver.com>
2025-09-22 20:38:05 +0900

    feat(Ecommerce) : Goods 컴포넌트 생성 & 데이터 바인딩 & 컴포넌트 별 디렉토리 생성

---
+++ Ecommerce/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// src/components/FooterCard.jsx
+/* 하단 카드 */
+export default function Footer() {
+  <div className="card m-5">
+    <div className="card-Header">Featured</div>
+    <div className="card-body">
+      <h5 className="card-title">Special title treatment</h5>
+      <p className="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </div>
+  </div>;
+}
